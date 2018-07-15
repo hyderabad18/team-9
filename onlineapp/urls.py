deeada0bb6d5d52e70e86e846ee15e60d4d1fbbd
RamaKrishnaKunda <k.ramakrishna93@gmail.com>
2018-07-15 10:39:07 +0530

    Created views for logout

--- onlineapp/urls.py
+++ onlineapp/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
    #  path('api/v1/colleges/<int:college_id>/students/<int:student_id>', StudentChangeRestView.as_view(), name = "student_details_rest")
 
     path('signup/', SignUpView.as_view(), name = 'signup'),
+    path('logout/', logout_user, name = 'logout'),
     path('login/', LoginView.as_view(), name = 'login'),
     path('firstpage/', BasepageView.as_view(), name = 'firstpage'),
     path('create_event/', csrf_exempt(CreateEventView.as_view()), name = 'create_event'),
